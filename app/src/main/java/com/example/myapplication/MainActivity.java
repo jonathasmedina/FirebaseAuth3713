d40434cb2a38a274b3bb1a47b5053d92ce5b848c
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2023-11-09 19:42:46 -0400

    -

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -39,43 +39,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String email = edEmail.getText().toString();
-
+                //enviar email para recuperação de senha
                 mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful())
+                        if (task.isSuccessful())
                             Toast.makeText(MainActivity.this, "Email de recuperação de senha enviado.", Toast.LENGTH_SHORT).show();
-
                     }
                 });
-
             }
         });
 
         btLogar.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 String email = edEmail.getText().toString();
                 String senha = edSenha.getText().toString();
+                //login + verifica (e envia) email
                 mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Usuário logado", Toast.LENGTH_SHORT).show();
-
-                            //verificar usuário
+                        if (task.isSuccessful()) {
+                            //verificar usuário logado
                             FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
 
-                            if(usuario.isEmailVerified()){
-
-                            }
-                            else{
+                            if (usuario.isEmailVerified()) {
+                                Toast.makeText(MainActivity.this, "Usuário logado", Toast.LENGTH_SHORT).show();
+                            } else {
                                 Toast.makeText(MainActivity.this, "Usuário não verificado.", Toast.LENGTH_SHORT).show();
                                 usuario.sendEmailVerification();
                             }
-                        }
-                        else{
+                        } else {
                             Toast.makeText(MainActivity.this, "Erro ao logar.", Toast.LENGTH_SHORT).show();
 
                         }
@@ -85,26 +79,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = edEmail.getText().toString();
                 String senha = edSenha.getText().toString();
-
+                //cria usuário
                 mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                       if(task.isSuccessful()){
-                           Toast.makeText(MainActivity.this, "Usuário criado", Toast.LENGTH_SHORT).show();
-                       }
-                       else{
-                           Toast.makeText(MainActivity.this, "Usuário NÃO foi criado.", Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Usuário criado", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Usuário NÃO foi criado.", Toast.LENGTH_SHORT).show();
 
-                       }
+                        }
                     }
                 });
-
             }
         });
     }
